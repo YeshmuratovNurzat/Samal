5d47edba73a13a558382508ca2ecceb09a1024be
Nurzat <nyeshmuratov@gmail.com>
2022-12-13 16:39:34 +0600

    add data class Shop

--- app/src/main/java/kz/fime/samal/data/models/ProductDetailed.kt
+++ app/src/main/java/kz/fime/samal/data/models/ProductDetailed.kt
@@ -1,6 +1,7 @@
 package kz.fime.samal.data.models
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 import kz.fime.samal.utils.extensions.Item
 
@@ -9,6 +10,7 @@ data class ProductDetailed(
     val name: String?,
     val description: String?,
     val properties: List<ProductProperties>?,
+    val shop: Shop,
     val categories: List<Item>,
     val price: Item?,
     val images: List<Image>?,
@@ -45,3 +47,12 @@ data class ProductProperties(
         return "$name: $value"
     }
 }
+
+data class Shop(
+    val uuid: String?,
+    val slug: String?,
+    val name: String?,
+    val address:String?,
+    val two_gis: String?,
+    val logo: String?
+)
