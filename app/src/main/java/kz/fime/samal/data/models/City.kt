ba8daa875afc2cde80f2eed5e342283bbeaf9f28
Nurzat <nyeshmuratov@gmail.com>
2023-01-04 15:55:40 +0600

    add parameters

--- app/src/main/java/kz/fime/samal/data/models/City.kt
+++ app/src/main/java/kz/fime/samal/data/models/City.kt
@@ -2,5 +2,10 @@ package kz.fime.samal.data.models
 
 data class City(
     val city_id: String,
-    val name: String
+    val name: String?,
+    val slug: String,
+    val timezone: String,
+    val latitude: String,
+    val longitude: String,
+
 )
--- app/src/main/java/kz/fime/samal/utils/Const.kt
+++ app/src/main/java/kz/fime/samal/utils/Const.kt
@@ -3,8 +3,8 @@ package kz.fime.samal.utils
 import kz.fime.samal.utils.decoration.GridSpacingItemDecoration
 import kz.fime.samal.utils.extensions.dpToPx
 
-//const val BASE_URL = "https://api.samal.market"
-const val BASE_URL = "https://api-dev.samalsauda.kz"
+const val BASE_URL = "https://api.samal.market"
+//const val BASE_URL = "https://api-dev.samalsauda.kz"
 const val API = "$BASE_URL/api/v1/"
 
 const val SHARED_PREFERENCES_NAME = "Samal"
