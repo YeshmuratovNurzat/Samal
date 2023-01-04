b4b3132d7a2066476fb49b18f80f542cf646e65e
Nurzat <nyeshmuratov@gmail.com>
2023-01-04 15:24:26 +0600

    add parameters class CartItem installment and priceWithPercent

--- app/src/main/java/kz/fime/samal/ui/cart/adapters/CartItem.kt
+++ app/src/main/java/kz/fime/samal/ui/cart/adapters/CartItem.kt
@@ -3,5 +3,7 @@ package kz.fime.samal.ui.cart.adapters
 data class CartItem(
     val count: Int = 0,
     val title: String?,
-    val price: Int = 0
+    val price: Int = 0,
+    val installment: Boolean = true,
+    var priceWithPercent: Float = 0f
 )
