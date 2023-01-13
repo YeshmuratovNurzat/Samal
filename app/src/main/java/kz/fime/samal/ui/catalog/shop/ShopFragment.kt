8ca0b79dfe4492fdd259abefdc76d1b98113d6ed
Nurzat <nyeshmuratov@gmail.com>
2023-01-13 16:55:03 +0600

    add shop phone

--- app/src/main/java/kz/fime/samal/ui/catalog/shop/ShopFragment.kt
+++ app/src/main/java/kz/fime/samal/ui/catalog/shop/ShopFragment.kt
@@ -1,5 +1,7 @@
 package kz.fime.samal.ui.catalog.shop
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
@@ -10,6 +12,7 @@ import kz.fime.samal.R
 import kz.fime.samal.databinding.FragmentShopDetailsBinding
 import kz.fime.samal.ui.base.observeState
 import kz.fime.samal.ui.catalog.adapters.CategoriesAdapter
+import kz.fime.samal.utils.EditTextUtils
 import kz.fime.samal.utils.binding.BindingFragment
 import kz.fime.samal.utils.extensions.InnerItem
 import kz.fime.samal.utils.extensions.getOrNull
@@ -20,6 +23,7 @@ import timber.log.Timber
 class ShopFragment: BindingFragment<FragmentShopDetailsBinding>(FragmentShopDetailsBinding::inflate) {
 
     private val viewModel: ShopViewModel by viewModels()
+    private var shopPhone : String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.run {
@@ -32,6 +36,12 @@ class ShopFragment: BindingFragment<FragmentShopDetailsBinding>(FragmentShopDeta
             }
             rvCategories.adapter = shopAdapter
 
+            phone.setOnClickListener {
+                val callIntent = Intent(Intent.ACTION_DIAL)
+                callIntent.data = Uri.parse("tel: $shopPhone")
+                startActivity(callIntent)
+            }
+
             val imagesAdapter = ImagesAdapter()
             setUpVpAdapter(vpImages, imagesAdapter, dotsImages)
 
@@ -39,6 +49,7 @@ class ShopFragment: BindingFragment<FragmentShopDetailsBinding>(FragmentShopDeta
                 it.result?.let {
                     tvName.text = it.getOrNull("name")
                     tvAddress.text = it.getOrNull("address")
+                    shopPhone = EditTextUtils.getPhoneMasked(it.getOrNull<String>("phone").toString())
                     imagesAdapter.submitList(it.getOrNull<List<InnerItem>>("images"))
                 }
             }, {}, {})
