4a8f4b8b95d8940196d078d1703ac31b7ce3b7f2
Nurzat <nyeshmuratov@gmail.com>
2023-02-27 19:16:06 +0600

    add textView pick_up_point and btnBack

--- app/src/main/java/kz/fime/samal/ui/profile/orders/OrderDetailFragment.kt
+++ app/src/main/java/kz/fime/samal/ui/profile/orders/OrderDetailFragment.kt
@@ -1,14 +1,20 @@
 package kz.fime.samal.ui.profile.orders
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.navigateUp
 import dagger.hilt.android.AndroidEntryPoint
 import kz.fime.samal.R
+import kz.fime.samal.data.base.State
 import kz.fime.samal.data.models.order_detail.OrderDetailResponse
+import kz.fime.samal.data.models.order_detail.Status
 import kz.fime.samal.databinding.FragmentOrderDetailBinding
+import kz.fime.samal.ui.base.observeEvent
 import kz.fime.samal.ui.base.observeState
 import kz.fime.samal.utils.binding.BindingFragment
 
@@ -23,7 +29,9 @@ class OrderDetailFragment: BindingFragment<FragmentOrderDetailBinding>(FragmentO
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.run {
+            //ibClose.setOnClickListener { findNavController().navigateUp() }
+        }
         loadOrderDetails()
         observeViewModel()
         initView()
@@ -49,13 +57,28 @@ class OrderDetailFragment: BindingFragment<FragmentOrderDetailBinding>(FragmentO
     private fun drawOrderDetail(orderDetail: OrderDetailResponse) {
         binding.run {
             statusTv.text = orderDetail.order.status.name
+
+            if(orderDetail.order.status.name == "Отменен"){
+                btnBack.visibility = View.VISIBLE
+                btnClose.visibility = View.GONE
+
+                btnBack.setOnClickListener {
+                    findNavController().navigateUp()
+                }
+            }else{
+                btnBack.visibility = View.GONE
+                btnClose.visibility = View.VISIBLE
+            }
+
             paymentTypeInfo.text = orderDetail.order.payment.name
-            paymentAmount.text = orderDetail.order.total_cost
+            paymentAmount.text = orderDetail.order.total_cost + "₸"
             deliveryTypeTv.text = orderDetail.order.delivery.name
             productsCount.text = "Товаров " + orderDetail.order_products.size
 
             if (orderDetail.order.delivery.delivery_id == 2) {
                 deliverAddress.text = orderDetail.client_address.street + ", дом" + orderDetail.client_address.house_number + ", кв " + orderDetail.client_address.apartment
+            }else{
+                deliverAddress.text = orderDetail.pick_up_point.address
             }
 
             val orderDetailAdapter = OrderDetailAdapter()
@@ -82,15 +105,19 @@ class OrderDetailFragment: BindingFragment<FragmentOrderDetailBinding>(FragmentO
                     isHide = true
                 }
             }
+
+            btnClose.setOnClickListener {
+                findNavController().navigate(R.id.action_global_deleteOrderDialog, bundleOf(Pair("id", orderDetail.order.order_uuid)))
+            }
         }
     }
 
     private fun initView() {
         val orderNumber = args.orderNumber
         binding.run {
-            toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
-            toolbar.title = "Заказ №$orderNumber"
+//            toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+//            toolbar.title = "Заказ №$orderNumber"
+            orderTitle.text = "Заказ №$orderNumber"
         }
     }
-
 }
