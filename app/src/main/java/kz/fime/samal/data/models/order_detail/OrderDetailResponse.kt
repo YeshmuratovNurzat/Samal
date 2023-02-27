361072f7aa4a822d5e2fa0cb2deafd32f48db137
Nurzat <nyeshmuratov@gmail.com>
2023-02-27 18:42:51 +0600

    add PickUpAddress

--- app/src/main/java/kz/fime/samal/data/models/order_detail/OrderDetailResponse.kt
+++ app/src/main/java/kz/fime/samal/data/models/order_detail/OrderDetailResponse.kt
@@ -1,8 +1,10 @@
 package kz.fime.samal.data.models.order_detail
 
+import kz.fime.samal.data.models.PickUpAddress
+
 data class OrderDetailResponse(
     val client_address: ClientAddress,
     val order: Order,
     val order_products: List<OrderProduct>,
-    val pick_up_point: Any
+    val pick_up_point: PickUpAddress
 )
