ae1160eda6c009a8da7ebb6675e4b0aaa5096807
Nurzat <nyeshmuratov@gmail.com>
2023-02-27 19:15:01 +0600

    add pagination

--- app/src/main/java/kz/fime/samal/ui/profile/orders/OrdersFragment.kt
+++ app/src/main/java/kz/fime/samal/ui/profile/orders/OrdersFragment.kt
@@ -1,13 +1,21 @@
 package kz.fime.samal.ui.profile.orders
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
+import kz.fime.samal.data.models.custom.Status
+import kz.fime.samal.data.models.order_detail.Order
 import kz.fime.samal.databinding.FragmentOrdersBinding
+import kz.fime.samal.ui.base.observeEvent
 import kz.fime.samal.ui.base.observeState
 import kz.fime.samal.utils.binding.BindingFragment
+import kz.fime.samal.utils.components.BottomBar
+import kz.fime.samal.utils.extensions.Item
 import kz.fime.samal.utils.extensions.getOrNull
 import timber.log.Timber
 
@@ -15,31 +23,107 @@ import timber.log.Timber
 class OrdersFragment: BindingFragment<FragmentOrdersBinding>(FragmentOrdersBinding::inflate) {
 
     private val viewModel: OrdersViewModel by viewModels()
+    private var objects: MutableList<Item> = mutableListOf()
+    private var isLoading = false
+    private var page: String = "0"
+    private var nextPage: String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.run {
             toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
 
-            viewModel.load()
-            srl.setOnRefreshListener { viewModel.load() }
+            contentEmpty.btnGoToCatalog.setOnClickListener {
+                BottomBar.getBottomBar()?.selectItemIndex(1)
+            }
+
+            srl.setOnRefreshListener {
+                srl.isRefreshing = false
+                //viewModel.load()
+            }
 
             val ordersAdapter = OrdersAdapter {
                 findNavController().navigate(OrdersFragmentDirections.toOrderDetailsFragment(it.getOrNull("order_uuid", "")!!, it.getOrNull("number", 0) as Int))
             }
             rvItems.adapter = ordersAdapter
-            viewModel.orders.observeState(viewLifecycleOwner, {
+
+
+            viewModel.resultOrders.observe(viewLifecycleOwner){
                 Timber.d("Orders: %s", it)
-                if (it.result.isNullOrEmpty()) {
-                    rvItems.visibility = View.GONE
-                    contentEmpty.root.visibility = View.VISIBLE
-                } else {
-                    rvItems.visibility = View.VISIBLE
-                    contentEmpty.root.visibility = View.GONE
+
+                when(it.status) {
+
+                    Status.LOADING -> {}
+
+                    Status.SUCCESS -> {
+                        it.data?.let {
+                            it.data?.let { it1 ->
+                                if (objects.containsAll(it1)) {
+                                } else {
+                                    objects.addAll(it1)
+                                }
+                            }
+                            ordersAdapter.submitList(objects)
+                            srl.isRefreshing = false
+                            isLoading = false
+                            nextPage = it.links.getOrNull("next", "")
+                            rvItems.addOnScrollListener(object :
+                                RecyclerView.OnScrollListener() {
+                                override fun onScrolled(
+                                    recyclerView: RecyclerView,
+                                    dx: Int,
+                                    dy: Int
+                                ) {
+                                    super.onScrolled(recyclerView, dx, dy)
+                                    if (dy > 0) {
+                                        val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager?
+                                        val visibleItemCount = linearLayoutManager?.childCount
+                                        val totalItemCount = linearLayoutManager?.itemCount
+                                        val pastVisibleItems = linearLayoutManager?.findFirstVisibleItemPosition()
+                                        if (nextPage != null) {
+                                            if (!isLoading) {
+                                                if (visibleItemCount != null && pastVisibleItems != null && totalItemCount != null) {
+                                                    if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                                                        val pages = nextPage!!.split("?")
+                                                        if (page.toInt() < pages[pages.lastIndex].substring(5).toInt()) {
+                                                            page = pages[pages.lastIndex].substring(5)
+                                                            viewModel.getOrders(page)
+                                                            srl.isRefreshing = true
+                                                            isLoading = true
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    }
                 }
-                ordersAdapter.submitList(it.result)
-            }, {}, { srl.isRefreshing = it })
+            }
 
+//            viewModel.orders.observeState(viewLifecycleOwner, {
+//                Timber.d("Orders: %s", it)
+//                if (it.result.isNullOrEmpty()) {
+//                    rvItems.visibility = View.GONE
+//                    contentEmpty.root.visibility = View.VISIBLE
+//                } else {
+//                    rvItems.visibility = View.VISIBLE
+//                    contentEmpty.root.visibility = View.GONE
+//                }
+//                //ordersAdapter.submitList(it.result)
+//            }, {}, { srl.isRefreshing = it })
 
+            viewModel.cancelOrder.observe(viewLifecycleOwner){
+                //viewModel.getOrders("")
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.cancelOrder.observeEvent(viewLifecycleOwner){
+            //viewModel.getOrders("")
         }
     }
 }
--- app/src/main/java/kz/fime/samal/ui/profile/orders/OrdersViewModel.kt
+++ app/src/main/java/kz/fime/samal/ui/profile/orders/OrdersViewModel.kt
@@ -1,20 +1,30 @@
 package kz.fime.samal.ui.profile.orders
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kz.fime.samal.api.ApiResponse
+import kz.fime.samal.data.models.OrderRequestModel
+import kz.fime.samal.data.models.custom.Resource
+import kz.fime.samal.data.models.order_detail.Order
 import kz.fime.samal.data.models.order_detail.OrderDetailResponse
 import kz.fime.samal.data.repositories.MainRepository
+import kz.fime.samal.data.repositories.ProfileRepository
 import kz.fime.samal.ui.base.BaseViewModel
 import kz.fime.samal.utils.extensions.Item
 import javax.inject.Inject
 
 @HiltViewModel
 class OrdersViewModel @Inject constructor(
-    private val mainRepository: MainRepository
+    private val mainRepository: MainRepository,
+    private val profileRepository: ProfileRepository
+
 ): BaseViewModel() {
 
-    private val getOrdersRequest = NetworkRequest{ mainRepository.getOrders() }
+    private val getOrdersRequest = NetworkRequest{mainRepository.getOrders()}
     val orders = getOrdersRequest.liveData
 
     private val getOrderDetailRequest = NetworkRequest<OrderDetailResponse>()
@@ -22,6 +32,16 @@ class OrdersViewModel @Inject constructor(
 
     fun load(){
         getOrdersRequest.call()
+    }
+
+    private val _loadOrders = MutableLiveData<OrdersRequestModel>()
+    val resultOrders: LiveData<Resource<ApiResponse<List<Item>>>> = Transformations
+        .switchMap(_loadOrders) {
+            profileRepository.getOrders(it.page)
+        }
+
+    fun getOrders(page: String?){
+        _loadOrders.postValue(OrdersRequestModel(page))
     }
 
     fun loadDetails(uuid: String) {
@@ -33,8 +53,23 @@ class OrdersViewModel @Inject constructor(
     private val placeOrderRequest = NetworkRequestEvent<Item>(CoroutineScope(Dispatchers.IO))
     val placeOrder = placeOrderRequest.liveData
 
+    private val cancelOrderRequest = NetworkRequestEvent<Nothing>(CoroutineScope(Dispatchers.IO), ::load)
+    val cancelOrder = cancelOrderRequest.liveData
+
+    fun cancelOrder(orderId: String) {
+        cancelOrderRequest.call {mainRepository.cancelOrder(orderId)}
+    }
+
+    data class OrdersRequestModel(
+        val page: String?
+    )
+
 //    fun placeOrder(){
 //        placeOrderRequest.call { mainRepository.placeOrder() }
 //    }
+
+    init {
+        getOrders("")
+    }
 
 }
